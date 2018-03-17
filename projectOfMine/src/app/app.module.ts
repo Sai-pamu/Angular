d08773b721a50cd097569633bf6e6c52e9d796f2
likhi chinnari <saipamu.uidev@gmail.com>
2018-03-17 16:34:40 -0500

    Component added

--- projectOfMine/src/app/app.module.ts
+++ projectOfMine/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CasualComponent } from './casual/casual.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasualComponent
   ],
   imports: [
     BrowserModule
